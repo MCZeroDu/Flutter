2dcfbfff494b9b67121d007e55eef41d52033512
QinSG <a13391509020@163.com>
2020-04-04 22:32:55 +0800

    部分修改

--- flutter_app1/lib/commpons/CategoryPage.dart
+++
@@ -1,8 +0,0 @@
-//引入ui库
-import 'package:flutter/material.dart';
-//创建组件
-// class CategryPage extends StatefulWidget{
-//   CategryPage({Key key}) : super(key:key);
-
-  
-// }
--- flutter_app1/lib/pages/farm.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget farmPages(){
-  return Container(
-    //  child:Text("Then Farm")
-  );
-}
